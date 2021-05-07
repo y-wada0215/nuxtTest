dd1514b394e54c17a2fbd99b831cbfdef53c3403
y-wada0215 <42237186+y-wada0215@users.noreply.github.com>
2021-05-08 04:38:30 +0900

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,38 @@
+export default {
+  components: true,
+  /*
+   ** Headers of the page
+   ** Doc: https://vue-meta.nuxtjs.org/api/#metainfo-properties
+   */
+  head: {
+    title: "Nuxt.js starter for CSB",
+    meta: [
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content: "Official Nuxt.js starter for CodeSandBox"
+      }
+    ],
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
+  },
+
+  /*
+   ** Nuxt.js modules
+   ** Doc: https://modules.nuxtjs.org
+   */
+  modules: [],
+
+  /*
+   ** Global CSS
+   ** Doc: https://nuxtjs.org/docs/2.x/configuration-glossary/configuration-css
+   */
+  css: ["~/assets/css/main"],
+
+  /*
+   ** Plugins to load before mounting the App
+   ** Doc: https://nuxtjs.org/docs/2.x/directory-structure/plugins
+   */
+  plugins: []
+};
